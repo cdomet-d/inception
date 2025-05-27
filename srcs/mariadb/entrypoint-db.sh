83aaecc1143bd40390264850645fab6853a25b37
Coralie Domet de Mont <cdomet-d@student.42lyon.fr>
2025-05-27 18:03:37 +0200

    chore: formatting & script improvements
    - Replaced UPDATE with ALTER USER in entrypoint-db
    - Added safety checks in entrypoit-wp

--- srcs/mariadb/entrypoint-db.sh
+++ srcs/mariadb/entrypoint-db.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #db-init.sh
 
-# exit on error [e], on unset variables [u]
 set -eu
 
 error() {
@@ -12,12 +11,11 @@ error() {
 [ -s "$MYSQL_ROOT_PASSWORD_FILE" ] || error "Empty or missing root password file"
 [ -s "$MYSQL_PASSWORD_FILE" ] || error "Empty or missing user password file"
 
-
 ROOT_PW="$(cat "$MYSQL_ROOT_PASSWORD_FILE")"
 USER_PW="$(cat "$MYSQL_PASSWORD_FILE")"
 
 if [ -z "$(ls -A /var/lib/mysql)" ]; then
-	cat <<- EOF
+	cat <<-EOF
 
 		Running MariaDB initialization...
 
@@ -25,25 +23,25 @@ if [ -z "$(ls -A /var/lib/mysql)" ]; then
 
 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-	mysqld --user=mysql --bootstrap <<- EOSQL
-	-- set root password
-	UPDATE mysql.user SET Password=PASSWORD('${ROOT_PW}')
-	-- remove any anonymous users
-	DELETE FROM mysql.user WHERE User='';
-	-- remove remote root access
-	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
-	-- delete default database test
-	DROP DATABASE IF EXISTS test
-	-- creating wp database & user from env
-	CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-	CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '${USER_PW}';
-	GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'wp-manager'@'%;
-	-- immediatly applies newly set priviledges
-	FLUSH PRIVILEGES;
+	mysqld --user=mysql --bootstrap <<-EOSQL
+		-- set root password
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PW}'
+		-- remove any anonymous users
+		DELETE FROM mysql.user WHERE User='';
+		-- remove remote root access
+		DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
+		-- delete default database test
+		DROP DATABASE IF EXISTS test
+		-- creating wp database & user from env
+		CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+		CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '${USER_PW}';
+		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'wp-manager'@'%;
+		-- immediatly applies newly set priviledges
+		FLUSH PRIVILEGES;
 
 	EOSQL
 
-	cat <<- EOF
+	cat <<-EOF
 
 		Sucessfully initialized ${MYSQL_DATABASE}
 		Successfully created user ${MYSQL_USER}
--- srcs/wordpress/entrypoint-wp.sh
+++ srcs/wordpress/entrypoint-wp.sh
@@ -3,30 +3,40 @@
 
 set -eu
 
+error() {
+	echo "FATAL: $*" >&2
+	exit 1
+}
+
+command -v curl >/dev/null 2>&1 || error "curl needed but not found"
+command -v tar >/dev/null 2>&1 || error "tar needed but not found"
+
+
 if [ -z "$(ls -A /var/www/html)" ]; then
-	cat <<- EOF
+	cat <<-EOF
 		Wordpress volume is empty. 
 		Downloading Wordpress from https://wordpress.org/latest.tar.gz...
 	EOF
-	curl -O https://wordpress.org/latest.tar.gz;
+	curl -fLO https://wordpress.org/latest.tar.gz || error "Failed to download Wordpress"
 
-	cat <<- EOF
+	cat <<-EOF
 		Extracting latest.tar.gz into Wordpress volume [/var/www/html]
 	EOF
 
-	tar -xvzf latest.tar.gz -C /var/www/html --strip-components=1;
+	tar -xvzf latest.tar.gz -C /var/www/html --strip-components=1 || error "Failed to extract WordPress"
 
-	cat <<- EOF
+	cat <<-EOF
 		Removing latest.tar.gz from $(pwd)
 	EOF
 	rm latest.tar.gz
 
-	cat <<- EOF
+	chown nginx:nginx
+	cat <<-EOF
 		Wordpress files ready at /var/www/html
 		# $(ls /var/www/html)
 	EOF
-	else
-		cat <<- EOF
+else
+	cat <<-EOF
 		Volume is already populated
 		Exiting...
 	EOF
